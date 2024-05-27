611570111562059de7653e4624a298993a834b4d
nodwick <rudolfh@mac.com>
2024-05-27 19:26:02 +0200

    completed bite 46 fizzbuzz

--- 46/fizzbuzz.py
+++ 46/fizzbuzz.py
@@ -2,4 +2,13 @@ from typing import Union
 
 
 def fizzbuzz(num: int) -> Union[str, int]:
-    pass
+    if num % 3 == 0 and num % 5 == 0:
+        return 'Fizz Buzz'
+    elif num % 3 == 0:
+        return 'Fizz'
+    elif num % 5 == 0:
+        return 'Buzz'
+    else:
+        return num
+
+print(fizzbuzz(23))
