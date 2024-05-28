86d50d6d54d240f2846eedaff870552d32dfff94
nodwick <rudolfh@mac.com>
2024-05-28 19:51:53 +0200

    completed bite 46 fizzbuzz incl. alternative

--- 67/calc_dts.py
+++ 67/calc_dts.py
@@ -1,3 +1,4 @@
+import datetime
 from datetime import date, timedelta
 
 start_100days = date(2017, 3, 30)
@@ -7,9 +8,11 @@ pycon_date = date(2018, 5, 8)
 
 def get_hundred_days_end_date():
     """Return a string of yyyy-mm-dd"""
-    pass
+    time_gap = timedelta(days=100)
+    return f'{start_100days + time_gap}'
 
 
 def get_days_between_pb_start_first_joint_pycon():
     """Return the int number of days"""
-    pass
+    return (pycon_date - pybites_founded).days
+
