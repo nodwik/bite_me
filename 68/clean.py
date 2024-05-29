27c6df35fe201e1256eb8616268d84717e209f78
nodwick <rudolfh@mac.com>
2024-05-29 20:25:48 +0200

    completed Bite 68. remove_punctuation

--- 68/clean.py
+++ 68/clean.py
@@ -1,3 +1,12 @@
+import re
 def remove_punctuation(input_string):
     """Return a str with punctuation chars stripped out"""
-    pass
+    #return re.sub(r'[!"#$%&\'()*+,-./:;<=>?@[\\\]^_`{|}~]', '', input_string)
+    mlist = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~'
+    return ''.join([char for char in input_string if char not in mlist])
+
+
+input_str = "Hello, world! How's it going?"
+result = remove_punctuation(input_str)
+print(result)  # Output: "Hello world Hows it going"
+
