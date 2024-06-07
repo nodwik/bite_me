a6940fa6a6acb20cc3d38315650fa243bae87684
nodwick <rudolfh@mac.com>
2024-06-07 22:29:42 +0200

    completed Bite 143. Look up a value in 3 dictionaries

--- 143/merge.py
+++ 143/merge.py
@@ -10,4 +10,20 @@ def get_person_age(name):
        If name in > 1 dict, return the match of the group with
        greatest N (so group3 > group2 > group1)
     """
-    pass
+    if name is None:
+        return NOT_FOUND
+
+    name = name.lower()
+    my_dict = [group1, group2, group3]
+    found_age = []
+    for group in reversed(my_dict):
+        lower_group = {k.lower(): v for k, v in group.items()}
+        if name in lower_group:
+            found_age.append(lower_group[name])
+
+    if len(found_age) >= 1:
+        return found_age[0]
+    else:
+        return NOT_FOUND
+
+get_person_age('ana')
