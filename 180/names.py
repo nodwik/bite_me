60f84930fab031bf2790f4ada0c5791bc0af681d
nodwick <rudolfh@mac.com>
2024-06-19 21:43:40 +0200

    completed Bite 180. names

--- 180/names.py
+++ 180/names.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+import pandas as pd
 
 # fake data from https://www.mockaroo.com
 data = """last_name,first_name,country_code
@@ -14,6 +15,26 @@ Braunle,Kermit,PL
 Halbard,Davie,CN"""
 
 
-def group_names_by_country(data: str = data) -> defaultdict:
+#def group_names_by_country(data: str = data) -> defaultdict:
+#    countries = defaultdict(list)
+#    lines = data.split('\n')[1:]
+#    for line in lines:
+#        last_name, first_name, ID = line.split(',')
+#        if ID in countries.keys():
+#            current = countries[ID]
+#            new = countries[ID] + [first_name + ' ' + last_name]
+#            countries[ID] = new
+#        else:
+#            countries[ID] = [first_name + ' ' + last_name]
+#    return countries
+
+def group_names_by_country(data: str) -> defaultdict:
     countries = defaultdict(list)
-    # your code
+    lines = data.strip().split('\n')[1:]  # Skip the header line and remove any leading/trailing whitespace
+    for line in lines:
+        last_name, first_name, ID = line.split(',')
+        countries[ID].append(f"{first_name} {last_name}")
+    return countries
+
+
+group_names_by_country(data)
