55b2fc51183eb11a5afe6483d3d26fce6bd2d822
nodwick <rudolfh@mac.com>
2024-06-20 21:58:07 +0200

    completed Bite 270. count digit frequency

--- 270/freq.py
+++ 270/freq.py
@@ -1,2 +1,47 @@
+# def freq_digit(num: int) -> int:
+#     my_dict = {}
+#     result = (0, 0)
+#
+#     for nr in str(num):
+#         if nr in my_dict.keys():
+#             my_dict[nr].append(nr)
+#         else:
+#             my_dict[nr] = [nr]
+#     for key, value in my_dict.items():
+#         if len(value) > result[0]:
+#             result = (len(value), int(key))
+#     print(result[1])
+#     print(my_dict)
+#     return result[1]
+
+
 def freq_digit(num: int) -> int:
-    pass
+    """
+    Finds the most frequent digit in a given integer.
+
+    Parameters:
+    num (int): The input number.
+
+    Returns:
+    int: The digit that appears most frequently in the input number.
+    """
+    my_dict = {}
+
+    # Count occurrences of each digit
+    for nr in str(num):
+        if nr in my_dict:
+            my_dict[nr] += 1
+        else:
+            my_dict[nr] = 1
+
+    # Find the digit with the maximum count
+    max_count = 0
+    max_digit = 0
+    for key, value in my_dict.items():
+        if value > max_count:
+            max_count = value
+            max_digit = int(key)
+
+    return max_digit
+
+freq_digit(994145467)
