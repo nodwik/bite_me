9812e34227dab4f4a0eb042768488987d743af18
nodwick <rudolfh@mac.com>
2024-06-20 21:29:07 +0200

    completed Bite 189. control flow

--- 189/control_flow.py
+++ 189/control_flow.py
@@ -3,5 +3,47 @@ QUIT_CHAR = 'q'
 MAX_NAMES = 5
 
 
+
+# def filter_names(names):
+#     return_list = []
+#     count_names = 0
+#     for name in names:
+#         if name[0] == IGNORE_CHAR:
+#             continue
+#         elif name[0] == QUIT_CHAR:
+#             break
+#         else:
+#             digits = 0
+#             for char in name:
+#                 if char.isdigit():
+#                     digits += 1
+#             if digits > 0:
+#                 continue
+#             else:
+#                 if count_names == MAX_NAMES:
+#                     break
+#                 else:
+#                     count_names += 1
+#                     return_list.append(name)
+#     return return_list
+
+
 def filter_names(names):
-    pass
+    return_list = []
+    count_names = 0
+
+    for name in names:
+        if name[0] == IGNORE_CHAR:
+            continue
+        if name[0] == QUIT_CHAR:
+            break
+        if any(char.isdigit() for char in name):
+            continue
+
+        return_list.append(name)
+        count_names += 1
+
+        if count_names == MAX_NAMES:
+            break
+
+    return return_list
