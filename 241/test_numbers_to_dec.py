88072f24926f49ab19b9fa2d4daf54f6323a96c0
nodwick <rudolfh@mac.com>
2024-06-23 19:10:42 +0200

    completed Bite 241. pytest numbers

--- 241/test_numbers_to_dec.py
+++ 241/test_numbers_to_dec.py
@@ -1,3 +1,21 @@
 import pytest
 
 from numbers_to_dec import list_to_decimal
+
+test_sample = {
+    's1' : ([0, 4, 2, 8], 428),
+    's2' : ([1, 2], 12),
+    's3' : ([3], 3),
+    's4' : ([6, 2, True], 'TypeError'),
+    's5' : ([-3, 12], 'ValueError'),
+    's6' : ([3.6, 4, 1], 'TypeError'),
+    's7' : (['4', 5, 3, 1], 'TypeError')
+}
+def test_numbers_to_dec():
+    for k, v in test_sample.items():
+        actual = list_to_decimal(v[0])
+        expected = v[1]
+        return actual == expected
+
+
+
